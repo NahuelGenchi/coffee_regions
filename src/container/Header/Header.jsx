fa00e1d6b2730fe7766c76ada5d3b363046c408c
Nahuel Genchi <nahuelgenchi@gmail.com>
2022-08-06 12:30:57 -0300

    improving header design

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -7,6 +7,7 @@ const Header = () => {
     <header className="section-header">
       <SectionContainer>
         <h1>The Best Coffee Regions</h1>
+        <p>Let's take a look at the countries with the highest quality of coffee beans.</p>
       </SectionContainer>
     </header>
   );
